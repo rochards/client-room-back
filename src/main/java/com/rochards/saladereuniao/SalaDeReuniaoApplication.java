a038a09637d28f1e08a740c03bd62d711e661cc7
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-15 17:53:44 -0300

    First commit.

---
+++ src/main/java/com/rochards/saladereuniao/SalaDeReuniaoApplication.java
@@ -0,0 +1,13 @@
+package com.rochards.saladereuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeReuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rochards/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rochards.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
