a8eb17ec7f45f41571a3f03e27049df21207b273
Rodrigo <rodrigorochasantos@outlook.com>
2021-01-18 09:35:04 -0300

    Created Room model.

---
+++ src/main/java/com/rochards/saladereuniao/model/Room.java
@@ -0,0 +1,27 @@
+package com.rochards.saladereuniao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+@Entity(name = "meeting_room")
+public class Room {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String startHour;
+
+    @Column(nullable = false)
+    private String endHour;
+}
